70949a6d91e5b5740e5747b700e3079e786cd17d
sezero <sezero@users.sourceforge.net>
2009-01-15 22:09:01 +0000

    added win64 cross-compilation script for mingw-w64
    git-svn-id: svn://svn.code.sf.net/p/vispatch/code/trunk@21 dfd6bf55-c3d9-46f4-9846-abe0cff2ce20

---
+++ build_w64.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+#
+# Change this script to meet your needs and/or environment.
+#
+
+TARGET=x86_64-pc-mingw32
+
+# change this to meet your setup:
+#PREFIX=/opt/buildbot/mingw-w64/slave-linux/linux-i386/build/root
+PREFIX=/opt/cross_win64
+
+PATH="$PREFIX/bin:$PREFIX/$TARGET/bin:$PATH"
+export PATH
+
+W64BUILD=1
+export W64BUILD
+
+CC="$TARGET-gcc"
+AS="$TARGET-as"
+AR="$TARGET-ar"
+WINDRES="$TARGET-windres"
+export CC WINDRES AS AR
+
+STRIPPER="$TARGET-strip"
+
+if [ "$1" = "strip" ]
+then
+$STRIPPER vispatch.exe
+exit 0
+fi
+
+HOST_OS=`uname|sed -e s/_.*//|tr '[:upper:]' '[:lower:]'`
+
+case "$HOST_OS" in
+linux)
+	MAKE_CMD=make
+	;;
+freebsd|openbsd|netbsd)
+	MAKE_CMD=gmake
+	;;
+*)
+	MAKE_CMD=make
+	;;
+esac
+
+exec $MAKE_CMD -f makefile.mingw $*
+
