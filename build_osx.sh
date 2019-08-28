0a4ab504898054622e254d90ecb513c0399dba6a
sezero <sezero@users.sourceforge.net>
2019-08-28 15:56:07 +0000

    added a makefile and a cross-build script to target Mac OS X
    git-svn-id: svn://svn.code.sf.net/p/vispatch/code/trunk@58 dfd6bf55-c3d9-46f4-9846-abe0cff2ce20

---
+++ build_osx.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+OLDPATH=$PATH
+MAKE_CMD=make
+
+rm -f vispatch.ppc vispatch.x86 vispatch.x64
+$MAKE_CMD -f makefile.darwin distclean
+
+# ppc
+TARGET=powerpc-apple-darwin9
+PATH=/opt/cross_osx-ppc/bin:$OLDPATH
+CC=$TARGET-gcc
+export PATH CC
+$MAKE_CMD -f makefile.darwin CROSS=$TARGET MACH_TYPE=ppc $* || exit 1
+$TARGET-strip -S vispatch || exit 1
+mv vispatch vispatch.ppc || exit 1
+$MAKE_CMD -f makefile.darwin distclean
+
+# x86
+TARGET=i686-apple-darwin9
+PATH=/opt/cross_osx-x86/bin:$OLDPATH
+CC=$TARGET-gcc
+export PATH CC
+$MAKE_CMD -f makefile.darwin CROSS=$TARGET MACH_TYPE=x86 $* || exit 1
+$TARGET-strip -S vispatch || exit 1
+mv vispatch vispatch.x86 || exit 1
+$MAKE_CMD -f makefile.darwin distclean
+
+# x86_64
+TARGET=x86_64-apple-darwin9
+PATH=/opt/cross_osx-x86_64/usr/bin:$OLDPATH
+CC=$TARGET-gcc
+export PATH CC
+$MAKE_CMD -f makefile.darwin CROSS=$TARGET MACH_TYPE=x86_64 $* || exit 1
+$TARGET-strip -S vispatch || exit 1
+mv vispatch vispatch.x64 || exit 1
+$MAKE_CMD -f makefile.darwin distclean
+
+echo "$TARGET-lipo -create -o vispatch vispatch.ppc vispatch.x86 vispatch.x64"
+$TARGET-lipo -create -o vispatch vispatch.ppc vispatch.x86 vispatch.x64 || exit 1
